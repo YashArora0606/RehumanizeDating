c0506e247b33c560c753f815d381f6c49fad952d
Yash Arora <31422627+YashArora0606@users.noreply.github.com>
2021-01-16 17:36:39 -0500

    vidochat

--- rehumanize/src/Routes.js
+++ rehumanize/src/Routes.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Landing from './components/Landing'
+import VideoChat from './components/VideoChat'
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Landing} />
+      <Route exact path="/video" component={VideoChat} />
+
     </Switch>
   )
 }
---
+++ rehumanize/src/components/VideoChat/config.js
@@ -0,0 +1,21 @@
+// Make a copy of this file and save it as config.js (in the src directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+const SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
+
+// OR, if you have not set up a web server that runs the learning-opentok-php code,
+// set these values to OpenTok API key, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tokbox.com/account.
+
+const API_KEY = '47084474';
+const SESSION_ID = '2_MX40NzA4NDQ3NH5-MTYxMDgyNDI3MTg1NX45c3MveWVER0d6aHIrQzBLeGFheEl0USt-fg';
+const TOKEN = 'T1==cGFydG5lcl9pZD00NzA4NDQ3NCZzaWc9MjcxNGU4MDFjNzM0MTVmNzIwNTNlY2ZiOWUxNzFkZThmOWI2N2RlNjpzZXNzaW9uX2lkPTJfTVg0ME56QTRORFEzTkg1LU1UWXhNRGd5TkRJM01UZzFOWDQ1YzNNdmVXVkVSMGQ2YUhJclF6QkxlR0ZoZUVsMFVTdC1mZyZjcmVhdGVfdGltZT0xNjEwODI0MzI1Jm5vbmNlPTAuMTU3MDYzMzU4NzExNTA2MjMmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTYxMzQxNjMyNCZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==';
+
+export {
+  SAMPLE_SERVER_BASE_URL,
+  API_KEY,
+  SESSION_ID,
+  TOKEN,
+};
